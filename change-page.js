ba47ddd1965363bdaf392c290bf87af7e16a740d
1u1ucas <lucasleveque17@gmail.com>
2024-01-18 17:28:15 +0100

    add a text for more information

--- change-page.js
+++ change-page.js
@@ -2,6 +2,7 @@ const map = document.querySelector(".map");
 const parent = document.querySelector(".parent");
 const pages = document.querySelectorAll(".grid-item");
 const JACardsText = document.querySelectorAll(".JA .card-text");
+const InfosUser = document.querySelector(".infos-user");
 
 const linearBtn = document.querySelector(".page-lineaire");
 
@@ -10,6 +11,8 @@ linearBtn.addEventListener("click", function () {
   parent.style.gap = "60px";
   pages.forEach((page) => (page.style.width = "80%"));
   JACardsText.forEach((JACardText) => (JACardText.style.bottom = "-20px"));
+  InfosUser.style.opacity = "0";
+  InfosUser.style.transition = "opacity 0s";
 });
 
 const freeBtn = document.querySelector(".page-libre");
@@ -19,4 +22,14 @@ freeBtn.addEventListener("click", function () {
   parent.style.gap = "none";
   pages.forEach((page) => (page.style.width = "auto"));
   JACardsText.forEach((JACardText) => (JACardText.style.bottom = "-80px"));
+  InfosUser.style.opacity = "1";
+  InfosUser.style.transition = "opacity 0s";
+  setTimeout(() => {
+    InfosUser.style.opacity = "0";
+    InfosUser.style.transition = "opacity 6s";
+  }, 1000);
 });
+
+setTimeout(function () {
+  InfosUser.classList.add("invisible");
+}, 1000);
